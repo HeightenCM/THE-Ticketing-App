153293bfb83c112976cb48227698c274a64f72d9
Constantin Marian Has <84171974+HeightenCM@users.noreply.github.com>
2023-11-12 21:31:45 +0200

    Some progress. Added getters/setters (no checks yet) for event class. Added (potential) other classes. Lots might change. Beta..?

--- THE Ticketing App/projclasslib.h
+++ THE Ticketing App/projclasslib.h
@@ -5,12 +5,75 @@
 
 using namespace std;
 
-enum class EventType{Movie, Play, Match};
+//struct Utility {
+//
+//};
+
+//class listsOfStuff {
+//
+//};
 
 struct Event {
+	string getName() {
+		return nameOfEvent;
+	}
+	bool setName(string nameOfEvent) {
+		this->nameOfEvent = nameOfEvent;
+	}
+
+	string getLocation() {
+		return locationOfEvent;
+	}
+	bool setLocation(string locationOfEvent) {
+		this->locationOfEvent = locationOfEvent;
+	}
+
+	char* getDate() {
+		//copystring func
+	}
+	bool setDate(const char* dateOfEvent) {
+		size_t lengthOfString = strlen(dateOfEvent) + 1;
+		this->dateOfEvent = new char[lengthOfString];
+		strcpy_s(this->dateOfEvent, lengthOfString, dateOfEvent);
+	}
+	
+	char* getTime() {
+		//copystring func
+	}
+	bool setTime(const char* openingTime) {
+		size_t lengthOfString = strlen(openingTime) + 1;
+		this->openingTime = new char[lengthOfString];
+		strcpy_s(this->openingTime, lengthOfString, openingTime);
+	}
+
+	int getRows() {
+		return noOfRows;
+	}
+	bool setRows(int noOfRows) {
+		this->noOfRows = noOfRows;
+	}
+	
+	int getSeats() {
+		return noOfSeats;
+	}
+	bool setSeats(int noOfSeats) {
+		this->noOfSeats = noOfSeats;
+	}
+	
+	int getZones() {
+		return noOfZones;
+	}
+	bool setZones(int noOfZones) {
+		this->noOfZones = noOfZones;
+	}
 
 private:
-	string nameOfEvent{};
-	char* date{};
+	string nameOfEvent{}, locationOfEvent{};
+	char* dateOfEvent{};
 	char* openingTime{};
+	int noOfRows{}, noOfSeats{}, noOfZones{};
 };
+
+//struct Ticket {
+//	
+//};
